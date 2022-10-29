6a43419767cb01703189bfc3dc57f06500faff90
caarlosdamian <dapecarlos@gmail.com>
2022-10-28 19:42:48 -0500

    feat: added new inputText component

--- src/components/textInput/TextInput.stories.tsx
+++ src/components/textInput/TextInput.stories.tsx
@@ -7,6 +7,29 @@ export default {
   argTypes: {},
 } as ComponentMeta<typeof TextInput>;
 
-const Template: ComponentStory<typeof TextInput> = (args) => <TextInput />;
+const Template: ComponentStory<typeof TextInput> = (args) => (
+  <TextInput {...args} />
+);
 
 export const Default = Template.bind({});
+Default.args = {
+  /* the args you need here will depend on your component */
+  error: false,
+  placeholder: 'Default...',
+};
+
+export const InputLabel = Template.bind({});
+InputLabel.args = {
+  /* the args you need here will depend on your component */
+  label: 'Default Input',
+  error: false,
+  placeholder: 'Label...',
+};
+
+export const Error = Template.bind({});
+Error.args = {
+  /* the args you need here will depend on your component */
+  label: 'Default Input',
+  error: true,
+  placeholder: 'Error...',
+};
--- src/components/textInput/TextInput.tsx
+++ src/components/textInput/TextInput.tsx
@@ -1,5 +1,29 @@
 import React from 'react';
+import './textInput.css';
 
-export const TextInput = () => {
-  return <div>TextInput</div>;
+interface TextInputProps extends React.HTMLProps<HTMLInputElement> {
+  label?: string;
+  error: boolean;
+  theme?: string;
+}
+
+export const TextInput = ({ ...props }: TextInputProps) => {
+  const { theme, error, label, id } = props;
+
+  return (
+    <div className="input-container">
+      {label && (
+        /* eslint-disable-next-line */
+        <label htmlFor={id} className="input-label">
+          Label Test
+        </label>
+      )}
+      <input
+        id={id}
+        className={`input-field ${theme} ${error ? 'input-error' : ''}`}
+        type="text"
+        {...props}
+      />
+    </div>
+  );
 };
