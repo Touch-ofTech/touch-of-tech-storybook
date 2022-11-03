11e7e4104ab6c300e9417696de1d534ae3082eb4
thisisofficial <thisisofficiallyofficial@gmail.com>
2022-11-02 20:32:44 -0600

    feat: grid  component and story added

---
+++ src/components/grid/Grid.tsx
@@ -0,0 +1,31 @@
+import React, { ReactNode } from 'react';
+import './grid.css';
+
+interface IGridProps {
+  className?: string;
+  template_rows?: string;
+  template_columns?: string;
+  gap?: string;
+  children?: ReactNode;
+}
+
+export const Grid = ({
+  className,
+  template_rows,
+  template_columns,
+  gap,
+  children,
+}: IGridProps) => {
+  return (
+    <div
+      className={`display-grid ${className}`}
+      style={{
+        gridTemplateColumns: template_columns,
+        gridTemplateRows: template_rows,
+        gap,
+      }}
+    >
+      {children}
+    </div>
+  );
+};
