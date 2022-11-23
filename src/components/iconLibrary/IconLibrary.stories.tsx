51f31f473f74e569a2df654c804f33fa681f8589
DiegoVillasenorVilla <divivi.619@gmail.com>
2022-11-23 00:53:18 -0600

    refactor(iconlibrary): some changes in iconlibrary stories

--- src/components/iconLibrary/IconLibrary.stories.tsx
+++ src/components/iconLibrary/IconLibrary.stories.tsx
@@ -1,5 +1,6 @@
 import { ComponentMeta, ComponentStory } from '@storybook/react';
 import { Icon } from './IconLibrary';
+import { iconsType } from '../../utils';
 import './iconStory.css';
 
 export default {
@@ -10,47 +11,14 @@ export default {
 const Template: ComponentStory<typeof Icon> = (args) => <Icon {...args} />;
 
 export const SingleIcon = Template.bind({});
-SingleIcon.args = {
-  iconName: 'spades',
-};
 
 export const IconLibrary = () => (
   <div className="container">
-    <div className="iconWrapper">
-      <Icon iconName="home" />
-      <span>home</span>
-    </div>
-    <div className="iconWrapper">
-      <Icon iconName="home2" />
-      <span>home2</span>
-    </div>
-    <div className="iconWrapper">
-      <Icon iconName="office" />
-      <span>office</span>
-    </div>
-    <div className="iconWrapper">
-      <Icon iconName="newspaper" />
-      <span>newspaper</span>
-    </div>
-    <div className="iconWrapper">
-      <Icon iconName="IE" />
-      <span>IE</span>
-    </div>
-    <div className="iconWrapper">
-      <Icon iconName="opera" />
-      <span>opera</span>
-    </div>
-    <div className="iconWrapper">
-      <Icon iconName="eyedropper" />
-      <span>eyedropper</span>
-    </div>
-    <div className="iconWrapper">
-      <Icon iconName="blog" />
-      <span>blog</span>
-    </div>
-    <div className="iconWrapper">
-      <Icon iconName="chrome" />
-      <span>chrome</span>
-    </div>
+    {iconsType.map((iconsVal) => (
+      <div className="iconWrapper" key={iconsVal.name}>
+        <Icon iconName={iconsVal.name} />
+        <span>{iconsVal.name}</span>
+      </div>
+    ))}
   </div>
 );
