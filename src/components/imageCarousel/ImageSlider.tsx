8d9947f83057745bb926eaa10e6ef9bc384bc6a7
caarlosdamian <dapecarlos@gmail.com>
2022-12-11 21:40:14 -0600

    fix(slider): check this

--- src/components/imageCarousel/ImageSlider.tsx
+++ src/components/imageCarousel/ImageSlider.tsx
@@ -1,101 +1,35 @@
-import React, { useState } from 'react';
-import Arrow from '../../assets/arrow.svg';
 import './imageSlider.css';
 
-const BtnSlider = ({
-  direction,
-  moveSlide,
-}: {
-  direction: 'next' | 'prev';
-  moveSlide: any;
-}) => {
-  return (
-    <button
-      type="button"
-      onClick={moveSlide}
-      className={`btn-slide ${direction}`}
-    >
-      <img src={Arrow} alt={direction} />
-    </button>
-  );
-};
-
-export const ImageSlider = ({
-  dots = true,
-  data,
-}: {
-  dots: boolean;
-  data: any[];
-}) => {
-  const [slideIndex, setSlideIndex] = useState(0);
-  const [prevSlideIndex, setPrevSlideIndex] = useState(1);
-
-  const changeSlide = (index: number | 'next' | 'prev') => {
-    setPrevSlideIndex(slideIndex);
-    if (typeof index === 'number') {
-      setSlideIndex(index);
+export const ImageSlider = ({ data }: { data: any[] }) => {
+  const nextslide = (index: number) => {
+    const slider = document.querySelector('.slider-container');
+    if (index === data.length - 1) {
+      slider.style.transform = 'translateX(0)';
     } else {
-      let newSlideIndex;
-      if (index === 'next') newSlideIndex = slideIndex + 1;
-      else if (index === 'prev') newSlideIndex = slideIndex - 1;
-      else newSlideIndex = 0;
-      setSlideIndex(newSlideIndex);
-      if (slideIndex > data.length - 1) setSlideIndex(0);
-      else if (slideIndex < 0) setSlideIndex(data.length - 1);
+      slider.style.transform = `translateX(-${(index + 1) * 800}px)`;
     }
   };
 
-  const nextChangeSlide = () => {
-    changeSlide('next');
-  };
-  const prevChangeSlide = () => {
-    changeSlide('prev');
-  };
-  const moveDot = (index: number) => {
-    changeSlide(index);
+  const prevSlide = (index: number) => {
+    const slider = document.querySelector('.slider-container');
+    if (index === 0) {
+      slider.style.transform = `translateX(-${(data.length - 1) * 800}px)`;
+    } else {
+      slider.style.transform = `translateX(-${(index - 1) * 800}px)`;
+    }
   };
 
   return (
     <div className="container-slider">
-      {data.map((slide, index) => {
-        let extraAttribute;
-        if (index === slideIndex) {
-          if (slideIndex > prevSlideIndex) extraAttribute = 'active-anim-rigth';
-          else extraAttribute = 'active-anim-left';
-        } else if (index === prevSlideIndex) {
-          if (slideIndex > prevSlideIndex)
-            extraAttribute = 'inactive-anim-rigth';
-          else extraAttribute = 'inactive-anim-left';
-        } else extraAttribute = 'hidden';
-        return (
-          <div
-            key={`slide${slide.title}`}
-            className={`slide ${extraAttribute}`}
-          >
-            <img src={slide.img} alt={slide.title} />
-            <div className="slide-text title">{slide.title}</div>
-            <div className="slide-text subtitle">{slide.subtitle}</div>
+      <div className="slider-container">
+        {data.map((item, index) => (
+          <div key={item.title} className="img-container">
+            <div className="circle" onClick={() => prevSlide(index)} />
+            <img src={item.img} alt={item.title} />
+            <div className="circle2" onClick={() => nextslide(index)} />
           </div>
-        );
-      })}
-      <BtnSlider moveSlide={nextChangeSlide} direction="next" />
-      <BtnSlider moveSlide={prevChangeSlide} direction="prev" />
-      {dots ? (
-        <div className="container-dots">
-          {data.map((slide, index) => (
-            <button
-              type="button"
-              key={`dot${slide.title}`}
-              onClick={() => moveDot(index)}
-              className={slideIndex === index ? 'dot active' : 'dot'}
-            >
-              {' '}
-            </button>
-          ))}
-        </div>
-      ) : (
-        ''
-      )}
+        ))}
+      </div>
     </div>
   );
 };
