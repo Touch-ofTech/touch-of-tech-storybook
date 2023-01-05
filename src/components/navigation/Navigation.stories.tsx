4e66c335775b3d5220ff011d87e5a96dd846477b
Erik Damian <thisisofficiallyofficial@gmail.com>
2023-01-04 23:05:04 -0600

    [MP-23]Navigation section component (#28)

---
+++ src/components/navigation/Navigation.stories.tsx
@@ -0,0 +1,18 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { Navigation } from './Navigation';
+
+export default {
+  title: 'Components/NavigationSection',
+  component: Navigation,
+  args: {
+    labels: ['Home', 'Our Story', 'Venue', 'Itinierary', 'Registry', 'RSVP'],
+    innitital1: 'C',
+    innitial2: 'M',
+  },
+} as ComponentMeta<typeof Navigation>;
+
+const Template: ComponentStory<typeof Navigation> = (args) => {
+  return <Navigation {...args} />;
+};
+
+export const Default = Template.bind({});
